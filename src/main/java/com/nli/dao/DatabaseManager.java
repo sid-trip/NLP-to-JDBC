02cac52e4381a275cfbf631a0b4a0e472efdc872
Siddhant <siddhant.tripathy.dev@gmail.com>
2026-03-09 22:33:53 +0530

    inmid: initializing package DatabaseManager

---
+++ src/main/java/com/nli/dao/DatabaseManager.java
@@ -0,0 +1,9 @@
+package com.nli.dao;
+import java.sql.*;
+import java.util.*;
+
+public class DatabaseManager {
+    private static final String URL = "jdbc:mysql://localhost:3306/vtu_project";
+    private static final String USER = "javauser";
+    private static final String PASS = "root";
+}
